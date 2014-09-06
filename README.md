7588a6ebb310358aa6b152df64cf7e8a4d3b2cd9
David P. Janes <davidjanes@davidjanes.com>
2014-09-06 07:31:08 -0400

    initial checkin

--- README.md
+++ README.md
@@ -2,3 +2,97 @@ iotdb-phant-client
 ==================
 
 Node Client for Phant
+
+## Usage
+To connect to [data.sparkfun.com](http://data.sparkfun.com)
+
+    var Phant = require('phant-client')
+    var phant = new Phant()
+
+To connect to your own [Phant](http://phant.io/) server:
+
+    var phant = new Phant({ "iri": "http://localhost:8080" })
+
+### Creating a new stream
+Functions
+<code>create()</code> and <code>connect()</code>
+will callback with a <code>streamd</code> object
+which can be used to do Phant stuff.
+You **must** <code>create()</code> or <code>connect()</code> to a Phant
+server first: you can't just use a serialized <code>streamd</code>
+object.
+
+    phant.create({
+        title: "My Awesome Stream",
+        description: "The description",
+        fields: "a,b,c"
+    }, function(error, streamd) {
+        if (error) {
+            console.log("+ error", error)
+        } else {
+            console.log(streamd)
+        }
+    }
+
+### Connecting to an existing stream
+
+    phant.connect(streamd, function(error, streamd) {
+        /* do stuff */
+    })
+
+
+### Adding a record
+
+
+    phant.add(streamd, {
+        a: 22
+    })
+    
+Phant requires that all fields be present. 
+<code>iotdb-phant-client</code> cleverly defaults
+these to the empty string.
+
+### Retrieving the latest record
+Note that this is entirely independent of the <code>next</code>/<code>reset</code>
+functions below.
+
+    phant.latest(streamd, function(error, rd) {
+        if (error) {
+            console.log("# error", error)
+        } else if (rd === null) {
+            console.log("+ eof")
+        } else {
+            console.log("+ got", rd)
+        }
+    })
+
+### Stepping through stream
+The first class to <code>next()</code> will get the first
+record and subsequent calls will step through the data.
+When no more data is available, <code>null</code> will be
+returned to indicate **EOF**. 
+If you want to start looping through the records again
+from the beginning, call <code>phant.reset(streamd)</code>. 
+If you need to loop through records multiple times
+simultaneously, use <code>phant.scrub(streamd)</code> to 
+return a copy of the stream object.
+
+The sightly complicated nature of this example
+is because the code is callback driven.
+
+    var fetcher = function() {
+        phant.next(streamd, function(error, rd) {
+            if (error) {
+                console.log("# error", error)
+            } else if (rd === null) {
+                console.log("+ eof")
+            } else {
+                console.log("+ got", rd)
+                fetcher()
+            }
+        })
+    }
+
+    fetcher()
+    
+
---
+++ phant.js
@@ -0,0 +1,400 @@
+/*
+ *  phant.js
+ *
+ *  David Janes
+ *  IOTDB.org
+ *  2014-09-05
+ *  "The Great Fire of London happened this day in 1666"
+ *
+ *  Connect to Phant/data.sparkfun.com-type servers
+ *
+ *  Copyright [2013-2014] [David P. Janes]
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+"use strict"
+
+var unirest = require('unirest')
+
+/**
+ *  Connect to a Phant server
+ *
+ *  @param {object} paramd
+ *  @param {String|undefined} paramd.iri
+ *  The server to connect to, by default "http://data.sparkfun.com"
+ *
+ *  @constructor
+ */
+var Phant = function(paramd) {
+    var self = this
+
+    paramd = defaults(paramd, {
+        iri: "http://data.sparkfun.com"
+    })
+
+    self.iri = paramd.iri
+}
+
+/**
+ *  Create a new stream
+ *
+ *  @param {object} paramd
+ *  @param {String} paramd.title
+ *  @param {String} paramd.description
+ *  @param {String} paramd.fields
+ *  
+ *  @param {Phant~stream_callback} callback
+ */
+Phant.prototype.create = function(paramd, callback) {
+    var self = this
+
+    paramd = defaults(paramd, {
+        title: "",
+        description: "",
+        fields: ""
+    })
+
+    unirest
+        .post(self.iri + "/streams")
+        .headers({ 'Accept': 'application/json' })
+        .type('json')
+        .send({
+            title: paramd.title,
+            description: paramd.description,
+            fields: paramd.fields
+        })
+        .end(function(result) {
+            if (result.error) {
+                if (result.body) {
+                    callback(result.body.message, null)
+                } else if (result.error.code) {
+                    callback(result.error.code, null)
+                } else {
+                    callback("error creating stream", null)
+                }
+            } else if (!result.body.success) {
+                callback(result.body.message, null)
+            } else {
+                callback(null, {
+                    title: result.body.stream.title,
+                    description: result.body.stream.description,
+                    fields: result.body.stream.fields,
+                    outputUrl: self.iri + '/output/' + result.body.publicKey,
+                    inputUrl: self.iri + '/input/' + result.body.publicKey,
+                    manageUrl: self.iri + '/streams/' + result.body.publicKey,
+                    publicKey: result.body.publicKey,
+                    privateKey: result.body.privateKey,
+                    deleteKey: result.body.deleteKey
+                })
+            }
+
+        })
+    ;
+}
+
+/**
+ *  Connect to a stream
+ *
+ *  @param {object} streamd
+ *  @param {String|undefined} streamd.publicKey
+ *  @param {String|undefined} streamd.privateKey
+ *  @param {String|undefined} streamd.deleteKey
+ *
+ *  @param {Phant~stream_callback} callback
+ */
+Phant.prototype.connect = function(streamd, callback) {
+    var self = this
+
+    unirest
+        .get(self.iri + '/streams/' + streamd.publicKey)
+        .headers({ 
+            'Accept': 'application/json',
+            // 'Phant-Private-Key': streamd.privateKey
+        })
+        // .type('json')
+        .end(function(result) {
+            if (result.error) {
+                if (result.body) {
+                    callback(result.body.message, null)
+                } else if (result.error.code) {
+                    callback(result.error.code, null)
+                } else {
+                    callback("error connecting to stream", null)
+                }
+            } else if (!result.body.success) {
+                callback(result.body.message, null)
+            } else {
+                callback(null, {
+                    title: result.body.stream.title,
+                    description: result.body.stream.description,
+                    fields: result.body.stream.fields,
+                    outputUrl: self.iri + '/output/' + result.body.publicKey,
+                    inputUrl: self.iri + '/input/' + result.body.publicKey,
+                    manageUrl: self.iri + '/streams/' + result.body.publicKey,
+                    publicKey: streamd.publicKey,
+                    privateKey: streamd.privateKey,
+                    deleteKey: streamd.deleteKey
+                })
+            }
+        })
+    ;
+}
+
+/**
+ *  Return the next record from the stream. If this
+ *  stream has not been read from it will return the
+ *  latest record first.
+ *  <p>
+ *  WARNING: If the stream is being written to while
+ *  this is being used, there's likely some duplicate 
+ *  records that will be returned when a refetch 
+ *  happens
+ *  <p>
+ *  This is likely to cache multiple results
+ *
+ *  @param {object} streamd
+ *  As returned by
+ *  {@link Phant#connect connect} or
+ *  {@link Phant#create create}.
+ *
+ *  @param {Phant~record_callback} callback
+ */
+Phant.prototype.next = function(streamd, callback) {
+    var self = this
+
+    var offset = 0
+
+    if (streamd.__rds === undefined) {
+        streamd.__rds = []
+        streamd.__offset = 0
+        streamd.__eof = false
+    } else if (streamd.__eof) {
+        callback(null, null)
+        return
+    } else if (streamd.__offset < streamd.__rds.length) {
+        callback(null, streamd.__rds[streamd.__offset++])
+        return
+    } else {
+        /* need to go deeper into the streamd */
+    }
+
+    /*
+     *  If we get here we need to fetch a new record
+     */
+    var iri = self.iri + '/output/' + streamd.publicKey + '.json?offset=' + streamd.__offset
+    unirest
+        .get(iri)
+        .headers({ 
+            'Accept': 'application/json',
+        })
+        .end(function(result) {
+            if (result.error) {
+                if (result.error.code) {
+                    callback(result.error.code, null)
+                } else {
+                    callback("error reading stream", null)
+                }
+            } else {
+                streamd.__rds.push.apply(streamd.__rds, result.body)
+                if (streamd.__offset < streamd.__rds.length) {
+                    callback(null, streamd.__rds[streamd.__offset++])
+                } else {
+                    streamd.__eof = true
+                    callback(null, null)
+                }
+            }
+        })
+    ;
+}
+
+/**
+ *  Reset the stream so that "next" will return
+ *  the latest record again.
+ *  <p>
+ *  This is immediate: there is no callback.
+ *
+ *  @param {object} streamd
+ *  As returned by
+ *  {@link Phant#connect connect} or
+ *  {@link Phant#create create}.
+ */
+Phant.prototype.reset = function(streamd) {
+    var self = this
+
+    streamd.__rds = undefined
+}
+
+/**
+ *  Return the latest record from the stream.
+ *  This is entirely independent of next/reset.
+ *  @param {object} streamd
+ *  As returned by
+ *  {@link Phant#connect connect} or
+ *  {@link Phant#create create}.
+ *
+ *  @param {Phant~record_callback} callback
+ */
+Phant.prototype.latest = function(streamd, callback) {
+    var self = this
+
+    unirest
+        .get(self.iri + '/output/' + streamd.publicKey + '/latest.json')
+        .headers({ 
+            'Accept': 'application/json',
+        })
+        .end(function(result) {
+            if (result.error) {
+                if (result.error.code) {
+                    callback(result.error.code, null)
+                } else {
+                    callback("error reading stream", null)
+                }
+            } else if (result.body) {
+                callback(null, result.body[0])
+            } else {
+                callback(null, null)
+            }
+        })
+    ;
+}
+
+/**
+ *  Add a record to the stream
+ *
+ *  @param {object} streamd
+ *  As returned by
+ *  {@link Phant#connect connect} or
+ *  {@link Phant#create create}.
+ *
+ *  @param {Phant~op_callback} callback
+ */
+Phant.prototype.add = function(streamd, rd, callback) {
+    var self = this
+
+    for (var fi in streamd.fields) {
+        var field = streamd.fields[fi]
+        if (rd[field] === undefined) {
+            rd[field] = ""
+        }
+    }
+    rd = defaults(rd, {})
+
+    unirest
+        .post(self.iri + '/input/' + streamd.publicKey)
+        .headers({ 
+            'Accept': 'application/json',
+            'Phant-Private-Key': streamd.privateKey
+        })
+        .type('json')
+        .send(rd)
+        .end(function(result) {
+            if (result.error) {
+                if (result.body) {
+                    callback(result.body.message, null)
+                } else if (result.error.code) {
+                    callback(result.error.code)
+                } else {
+                    callback("error adding to stream")
+                }
+            } else if (!result.body.success) {
+                callback(result.body.message)
+            } else {
+                callback(null)
+            }
+        })
+    ;
+}
+
+/**
+ *  Return a scrubbed version of the streamd.
+ *  These can be used for serialization to disk
+ *  or for starting an independent input stream.
+ *  <p>
+ *  A scrubbed streamd can be operated on
+ *  if it's the result of a 
+ *  {@link Phant#connect connect} or
+ *  {@link Phant#create create} callback.
+ *  <p>
+ *  This is immediate: there is no callback.
+ *
+ *  @param {object} streamd
+ *  As returned by
+ *  {@link Phant#connect connect} or
+ *  {@link Phant#create create}.
+ *
+ *  @return {object}
+ *  A new streamd
+ */
+Phant.prototype.scrub = function(streamd) {
+}
+
+/**
+ *  Make sure a 'paramd' is properly set up. That is,
+ *  that it's a dictionary and if any values in defaultd
+ *  are undefined in paramd, they're copied over
+ */
+var defaults = function(paramd, defaultd) {
+    if (paramd === undefined) {
+        return defaultd
+    }
+
+    for (var key in defaultd) {
+        var pvalue = paramd[key]
+        if (pvalue === undefined) {
+            paramd[key] = defaultd[key]
+        }
+    }
+
+    return paramd;
+}
+
+/**
+ *  Callback that returns a stream
+ *
+ *  @param {string} error
+ *  If not null, the error.
+ *
+ *  @param {object|undefined}
+ *  An open stream
+ *  Ignore if <code>error</code> is present
+ *
+ *  @callback Phant~stream_callback
+ */
+
+/**
+ *  Callback that returns a record from a stream
+ *
+ *  @param {string} error
+ *  If not null, the error
+ *
+ *  @param {object|null|undefined}
+ *  If null, this means EOF - no more
+ *  records are available. Otherwise
+ *  it's a record.
+ *  Ignore if <code>error</code> is present
+ *
+ *  @callback Phant~record_callback
+ */
+
+/**
+ *  Callback from a operations that have no result
+ *
+ *  @param {string} error
+ *  If null, the operation succeeed. Otherwise 
+ *  it's the operation.
+ *
+ *  @callback Phant~op_callback
+ */
+
+exports.Phant = Phant
